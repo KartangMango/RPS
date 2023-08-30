04d6536965c722ea5ea7217e341eb9a336aab4ed
Kartang <62968885+KartangMango@users.noreply.github.com>
2023-08-30 19:52:26 +0930

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,31 @@
+body {
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  text-align: center;
+}
+
+p {
+  text-align: center;
+}
+
+input[type="radio"] {
+  display: block;
+  margin: 0 auto;
+  width: 20%;
+}
+
+button {
+  background-color: green;
+  color: white;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: lightgreen;
+}
